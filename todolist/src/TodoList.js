fc77055dea39e98267864f645f0acab494afd2ed
MatthewBurtQA <matthewaaronburt@gmail.com>
2020-09-09 15:00:32 +0100

    now the array population and button function

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -20,9 +20,32 @@ import React, { Component } from "react";
     
      }
      // below is defining the add item event handler
+     // time to add some funk-tionality doo doo ba doo dooo.
+
      addItem(e){
+         //check input for empty string, then,
+         //add input element with data : text, and date
+         //ITEM IS OBJECT. ADVANCED DATA TYPE, CONTAINS TEXT AND DATE.
+         //text is content, date is key. 
+         if (this._inputElement.value !== "" ){
+            var newItem = {
+                text: this._inputElement.value,
+                key: Date.now()
+            };
+
+            this.setState((prevState) => {
+                return {
+                    items: prevState.items.concat(newItem)
+                };
+            });
+            this._inputElement.value ="";
+
+         }
+         console.log(this.state.items);
+         e.preventDefault(); // stops page refreshing on button submission, which is its default behaviour. 
 
      }
+     //access inputs via _inputElement
 
   render() {
     return (
