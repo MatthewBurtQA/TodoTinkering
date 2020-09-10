8c49acb2ac5390e6335c55a26dbade41f8b2aeec
MatthewBurtQA <matthewaaronburt@gmail.com>
2020-09-10 13:52:53 +0100

    Ty github for absolutely ruining my project

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,65 +1,66 @@
-import React, { Component } from "react";
- // Below is a div, and some input placeholder with button. this will then get called into the index JS to be called onto the screen.
+import React, { Component } from "react"; 
+import TodoItems from "./TodoItems";
 
- //need a constructor 
+class TodoList extends Component { 
+  constructor(props) {    
+    super(props);     
 
- 
- 
+    this.state = {    
+      items: []  
+    };
 
- class TodoList extends Component {
-    
-    constructor(props) {
-        
-        super(props);
-        
-        this.state={
-          items: []  // declaring an array of items. saves inputs to item array 
-        };
-        
-        this.addItem = this.addItem.bind(this);
-    
-     }
-     // below is defining the add item event handler
-     // time to add some funk-tionality doo doo ba doo dooo.
-
-     addItem(e){
-         //check input for empty string, then,
-         //add input element with data : text, and date
-         //ITEM IS OBJECT. ADVANCED DATA TYPE, CONTAINS TEXT AND DATE.
-         //text is content, date is key. 
-         if (this._inputElement.value !== "" ){
-            var newItem = {
-                text: this._inputElement.value,
-                key: Date.now()
-            };
-
-            this.setState((prevState) => {
-                return {
-                    items: prevState.items.concat(newItem)
-                };
-            });
-            this._inputElement.value ="";
-
-         }
-         console.log(this.state.items);
-         e.preventDefault(); // stops page refreshing on button submission, which is its default behaviour. 
-
-     }
-     //access inputs via _inputElement
-
-  render() {
-    return (
-      <div className="todoListMain">
-        <div className="header">
-          <form onSubmit={this.addItem}>
-            <input ref={(a) => this._inputElement = a} placeholder="Task?">
-            </input>
-            <button type="submit">add</button>
-          </form>
-        </div>
-      </div>
-    );
+    this.addItem = this.addItem.bind(this); 
+    this.deleteItem = this.deleteItem.bind(this);
   }
-}
- 
+
+  addItem(e) {
+    var itemArray = this.state.items;
+
+    if (this._inputElement.value !== "") {    
+      itemArray.unshift(
+        {      
+          text: this._inputElement.value,      
+          key: Date.now()    
+        }
+      );
+
+      this.setState({      
+        items: itemArray    
+      });   
+
+      this._inputElement.value = "";  
+    }   
+    console.log(itemArray);   
+    e.preventDefault();
+  }
+
+  deleteItem(key) {  
+    var filteredItems = this.state.items.filter(function (item) {    
+      return (item.key !== key);  
+    }); 
+
+    this.setState({    
+      items: filteredItems  
+    });
+  }
+
+  render() {    
+    return (      
+      <div className="todoListMain">        
+        <div className="header">          
+          <form onSubmit={this.addItem}>            
+            <input ref={(a) => this._inputElement = a} 
+              placeholder="enter task">
+            </input>            
+            <button type="submit">add</button>          
+          </form>        
+        </div>
+
+        <TodoItems entries={this.state.items} 
+                  delete={this.deleteItem}/>     
+      </div>    
+    );  
+  }
+} 
+
 export default TodoList;
