98e6d5343c65b01e3cbf67a95521d6845d1928e1
MatthewBurtQA <matthewaaronburt@gmail.com>
2020-09-10 13:08:06 +0100

    reworked the todo list, since it didn't work.

--- todolist/src/TodoItems.js
+++ todolist/src/TodoItems.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
- //this JS class returns a LIST of items. the KEY is the date. the TEXT is the content. 
+ 
 class TodoItems extends Component {
-  createTasks(item) {
-    return <li key={item.key}>{item.text}</li>
-  }
- //render a list of entries, using the "props" from the ToDoList. 
+    createTasks(item) {
+        return <li key={item.key}>{item.text}</li>
+      }
+ 
   render() {
     var todoEntries = this.props.entries;
     var listItems = todoEntries.map(this.createTasks);
